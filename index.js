45feae82abfe12d55612b7520a1f312b96323cc0
Justin Poliachik <jpoliachik@gmail.com>
2022-01-14 11:18:40 -0500

    remove semicolons

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const userDataPath = process.argv[2];
-const env = process.argv[3];
+const userDataPath = process.argv[2]
+const env = process.argv[3]
 
-const main = require('./dist/main');
+const main = require('./dist/main')
 
 module.exports.ClientBackend = (channel, userDataPath, env) => {
-  main({ channel, userDataPath, env });
-};
+  main({ channel, userDataPath, env })
+}
 
 if (env || userDataPath) {
-  this.ClientBackend(process, userDataPath, env);
+  this.ClientBackend(process, userDataPath, env)
 }
--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -20,27 +20,35 @@ export class AccountModel {
     this._collection = await this._drive.db.collection('Account')
   }
 
-  public async insert(doc: AccountSchema) : Promise<AccountSchema> {
+  public async insert(doc: AccountSchema): Promise<AccountSchema> {
     return this._collection.insert(doc)
   }
 
-  public async find(doc?: any) : Promise<AccountSchema> {
+  public async find(doc?: any): Promise<AccountSchema> {
     return this._collection.find(doc)
   }
 
-  public async findOne(doc?: any) : Promise<AccountSchema> {
+  public async findOne(doc?: any): Promise<AccountSchema> {
     return this._collection.findOne(doc)
   }
 
   // public async update(doc: AccountSchema) {
-    
+
   // }
 
   // public async search(doc: AccountSchema) {
-    
+
   // }
 
-  public async setVault(password: string, type: 'recovery' | 'vault', payload: { master_pass?: string, drive_encryption_key?: any, keyPair?: any }) {
+  public async setVault(
+    password: string,
+    type: 'recovery' | 'vault',
+    payload: {
+      master_pass?: string
+      drive_encryption_key?: any
+      keyPair?: any
+    },
+  ) {
     const memStream = new MemStream()
     const cipher = this._encrypt(JSON.stringify(payload), password)
 
@@ -49,11 +57,14 @@ export class AccountModel {
     return this._drive.writeFile(`/${type}`, memStream, { encrypted: false })
   }
 
-  public getVault(password: string, type: 'recovery' | 'vault') : { drive_encryption_key: any, keyPair: any } {
+  public getVault(
+    password: string,
+    type: 'recovery' | 'vault',
+  ): { drive_encryption_key: any; keyPair: any } {
     const vaultPath = path.join(`${this._store.acctPath}/Drive/Files/`, type)
-  
-    if(!fs.existsSync(vaultPath)) throw `${type} file not found.`
-    
+
+    if (!fs.existsSync(vaultPath)) throw `${type} file not found.`
+
     const cipher = fs.readFileSync(vaultPath)
 
     const deciphered = this._decrypt(cipher, password)
@@ -63,12 +74,20 @@ export class AccountModel {
 
   private _encrypt(msg: string, password: string) {
     const keyPair = this._keypairFromStr(password)
-    return this._encryptPubSecretBoxMessage(msg, keyPair.publicKey, keyPair.privateKey)
+    return this._encryptPubSecretBoxMessage(
+      msg,
+      keyPair.publicKey,
+      keyPair.privateKey,
+    )
   }
 
-  private _decrypt(cipher: string, password: string) : string {
+  private _decrypt(cipher: string, password: string): string {
     const keyPair = this._keypairFromStr(password)
-    const deciphered = this._decryptPubSecretBoxMessage(cipher, keyPair.publicKey, keyPair.privateKey)
+    const deciphered = this._decryptPubSecretBoxMessage(
+      cipher,
+      keyPair.publicKey,
+      keyPair.privateKey,
+    )
     return deciphered.toString('utf-8')
   }
 
@@ -78,40 +97,40 @@ export class AccountModel {
     const n = Buffer.alloc(sodium.crypto_box_NONCEBYTES)
     const pk = Buffer.alloc(sodium.crypto_box_PUBLICKEYBYTES)
     const sk = Buffer.alloc(sodium.crypto_box_SECRETKEYBYTES)
-  
+
     pk.fill(Buffer.from(sbpkey, 'hex'))
     sk.fill(Buffer.from(privKey, 'hex'))
-  
+
     sodium.crypto_box_easy(c, m, n, pk, sk)
-  
+
     return c
   }
 
   private _decryptPubSecretBoxMessage(c: string, pk: any, sk: any) {
     const m = Buffer.alloc(c.length - sodium.crypto_box_MACBYTES)
     const n = Buffer.alloc(sodium.crypto_box_NONCEBYTES)
-  
+
     const bool = sodium.crypto_box_open_easy(m, c, n, pk, sk)
-  
+
     if (!bool) throw new Error('Unable to decrypt message.')
-  
+
     return m
   }
 
-  private _keypairFromStr(str: string) : { publicKey: any, privateKey: any } {
+  private _keypairFromStr(str: string): { publicKey: any; privateKey: any } {
     let pk = Buffer.alloc(sodium.crypto_box_PUBLICKEYBYTES)
     let sk = Buffer.alloc(sodium.crypto_box_SECRETKEYBYTES)
     let seed = Buffer.alloc(sodium.crypto_box_SEEDBYTES)
-  
+
     const buf = Buffer.from(blake.blake2sHex(str))
-  
+
     seed.fill(buf)
-  
+
     sodium.crypto_box_seed_keypair(pk, sk, seed)
-  
+
     return {
       publicKey: pk,
-      privateKey: sk
+      privateKey: sk,
     }
   }
 }
