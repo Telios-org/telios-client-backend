d80f1fe52fb9eb5ae3888445066ed65ad4d58d09
hexadecibal <hex#github@telios.io>
2022-12-03 13:04:35 -0500

    debugging

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -424,15 +424,15 @@ export default async (props: EmailOpts) => {
                   Email.insert(_email)
                     .then(async (eml: EmailSchema) => {
                       
-                      setTimeout(() => {
-                        if(_email.aliasId) {
-                          Alias.update({ aliasId: _email.aliasId }, { $inc: { count: 1 }})
-                          store.setFolderCount(_email.aliasId, 1)
-                        } else {
-                          Folder.update({ folderId: _email.folderId }, { $inc: { count: 1 } })
-                          store.setFolderCount(_email.folderId, 1)
-                        }
-                      }, 1000)
+                      //setTimeout(() => {
+                        // if(_email.aliasId) {
+                        //   await Alias.update({ aliasId: _email.aliasId }, { $inc: { count: 1 }})
+                        //   store.setFolderCount(_email.aliasId, 1)
+                        // } else {
+                        //   await Folder.update({ folderId: _email.folderId }, { $inc: { count: 1 } })
+                        //   store.setFolderCount(_email.folderId, 1)
+                        // }
+                      //}, 1000)
                       resolve(eml)
                     })
                     .catch((err: any) => {
